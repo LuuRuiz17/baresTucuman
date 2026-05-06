3f4d30840d47b5f1dbd696bd183467e7980c5c67
Luciana Ruiz <lucianaruiz.934@gmail.com>
2026-05-05 23:52:29 -0300

    Implementé la lógica del CREATE em server.js

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+let bares = [];
+
+const app = express();
+app.use(express.json());
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
+
+// CREATE
+app.post("/bares", (req, res) => {
+    const nuevoBar = {
+        id: Date.now(),
+        nombre: req.body.nombre,
+        ubicacion: req.body.ubicacion,
+        categoria: "",
+        fuente: "mock",
+        fechaObtencion: new Date().toISOString(),
+        activo: true
+    };
+
+    bares.push(nuevoBar);
+
+    res.json(nuevoBar);
+});
+
+
